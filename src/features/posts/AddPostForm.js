c5dfbe08dd91bf95780fda7de08e4de3fdbd1418
klequis <klequis@gmail.com>
2020-08-11 15:31:43 -0700

    section done. added disabled=!canSave

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -56,7 +56,7 @@ export const AddPostForm = () => {
           value={content}
           onChange={onContentChanged}
         />
-        <button type="button" onClick={onSavePostClicked}>
+        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
           Save Post
         </button>
       </form>
