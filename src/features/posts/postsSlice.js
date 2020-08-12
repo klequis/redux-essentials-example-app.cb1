fb59bcca1688c7146eb924b9565acc68495078b5
klequis <klequis@gmail.com>
2020-08-12 10:20:06 -0700

    added all diffs from chapter end code

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,5 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { nanoid } from '@reduxjs/toolkit'
+import { createSlice, nanoid } from '@reduxjs/toolkit'
 import { sub } from 'date-fns'
 
 const initialState = [
@@ -7,6 +6,7 @@ const initialState = [
     id: '1',
     title: 'First Post!',
     content: 'Hello!',
+    user: '0',
     date: sub(new Date(), { minutes: 10 }).toISOString(),
     reactions: {
       thumbsUp: 0,
@@ -20,6 +20,7 @@ const initialState = [
     id: '2',
     title: 'Second Post',
     content: 'More text',
+    user: '2',
     date: sub(new Date(), { minutes: 5 }).toISOString(),
     reactions: {
       thumbsUp: 0,
@@ -35,13 +36,6 @@ const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    reactionAdded(state, action) {
-      const { postId, reaction } = action.payload
-      const existingPost = state.find(post => post.id === postId)
-      if (existingPost) {
-        existingPost.reactions[reaction]++
-      }
-    },
     postAdded: {
       reducer(state, action) {
         state.push(action.payload)
@@ -54,9 +48,23 @@ const postsSlice = createSlice({
             title,
             content,
             user: userId,
+            reactions: {
+              thumbsUp: 0,
+              hooray: 0,
+              heart: 0,
+              rocket: 0,
+              eyes: 0,
+            },
           },
         }
       },
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload
+      const existingPost = state.find((post) => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
+      }
     },
     postUpdated(state, action) {
       const { id, title, content } = action.payload
