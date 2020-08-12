d0c514bcf033e52f1e3086ee01f88ec6b3effbd7
klequis <klequis@gmail.com>
2020-08-12 07:22:17 -0700

    section done

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -9,12 +9,11 @@ export const AddPostForm = () => {
   const [userId, setUserId] = useState('')
 
   const dispatch = useDispatch()
-
   const users = useSelector((state) => state.users)
 
   const onTitleChanged = (e) => setTitle(e.target.value)
   const onContentChanged = (e) => setContent(e.target.value)
-  const onAuthorChanged = e => setUserId(e.target.value)
+  const onAuthorChanged = (e) => setUserId(e.target.value)
 
   const onSavePostClicked = () => {
     if (title && content) {
@@ -26,7 +25,7 @@ export const AddPostForm = () => {
 
   const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
 
-  const usersOptions = users.map(user => (
+  const usersOptions = users.map((user) => (
     <option key={user.id} value={user.id}>
       {user.name}
     </option>
@@ -41,6 +40,7 @@ export const AddPostForm = () => {
           type="text"
           id="postTitle"
           name="postTitle"
+          placeholder="What's on your mind?"
           value={title}
           onChange={onTitleChanged}
         />
